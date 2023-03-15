44d59c9368e79d0e785a3fb0c8bb34cd4ae3f730
zcmn <zhecaman@icloud.com>
2023-03-15 14:52:21 +0300

    linter wars episode 3

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,18 +3,18 @@ import random
 
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
+
 def isprime(number):
-    for i in range(2, (number//2) + 1):
+    for i in range(2, (number // 2) + 1):
         if number % i == 0:
             return False
     return True
-
 
 
 def get_question_and_correct_answer():
     '''
     Returns a tuple of question and correct answer
     '''
-    question = random.randint(1, 20)
+    question = random.randint(2, 20)
     correct_answer = 'yes' if isprime(question) else 'no'
     return question, correct_answer
